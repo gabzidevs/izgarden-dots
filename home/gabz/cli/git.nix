6317f3d76ef379e9958ca21876a76ebe5ca0af6e
gabzidevs <71532830+gabzidevs@users.noreply.github.com>
2025-10-26 10:54:31 +0800

    refactor(cli): update git configuration options

--- home/gabz/cli/git.nix
+++ home/gabz/cli/git.nix
@@ -34,7 +34,7 @@ in
     })
 
     (mkIf config.garden.profiles.workstation.git.fsck.enable {
-      programs.git.extraConfig = {
+      programs.git.settings = {
         # prevent data corruption
         transfer.fsckObjects = true;
         fetch.fsckObjects = true;
@@ -48,8 +48,6 @@ in
 
       programs.git = {
         package = pkgs.gitMinimal;
-        userName = "gabz";
-        userEmail = "gabz" + "@" + "spacehound"; # obsfuscated
 
         includes = [
           {
@@ -81,31 +79,6 @@ in
           signByDefault = true;
         };
 
-        aliases = {
-          st = "status";
-          br = "branch";
-          c = "commit -m";
-          ca = "commit -am";
-          co = "checkout";
-          d = "diff";
-          df = "!git hist | peco | awk '{print $2}' | xargs -I {} git diff {}^ {}";
-          fuck = "commit --amend -m";
-          graph = "log --all --decorate --graph";
-          ps = "!git push origin $(git rev-parse --abbrev-ref HEAD)";
-          pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
-          af = "!git add $(git ls-files -m -o --exclude-standard | fzf -m)";
-          hist = ''
-            log --pretty=format:"%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)" --graph --date=relative --decorate --all
-          '';
-          llog = ''
-            log --graph --name-status --pretty=format:"%C(red)%h %C(reset)(%cd) %C(green)%an %Creset%s %C(yellow)%d%Creset" --date=relative
-          '';
-          # https://github.com/arichtman/nix/blob/18f5613c2842e12e49350aeceace63863ad59244/modules/home/default-home/default.nix#L11
-          fuggit = "!git add . && git commit --amend --no-edit && git push --force";
-          # thanks @vbde for this
-          idc = "!git commit -am '$(curl -s https://whatthecommit.com/index.txt)'";
-        };
-
         ignores = [
           # system residue
           ".cache/"
@@ -135,18 +108,37 @@ in
           "vendor"
         ];
 
-        # pager / diff tool
-        delta = {
-          enable = true;
-
-          options = {
-            navigate = true;
-            side-by-side = true;
-            line-numbers = true;
+        settings = {
+          user = {
+            name = "gabz";
+            email = "gabz" + "@" + "spacehound"; # obsfuscated
           };
-        };
 
-        extraConfig = {
+          alias = {
+            st = "status";
+            br = "branch";
+            c = "commit -m";
+            ca = "commit -am";
+            co = "checkout";
+            d = "diff";
+            df = "!git hist | peco | awk '{print $2}' | xargs -I {} git diff {}^ {}";
+            fuck = "commit --amend -m";
+            graph = "log --all --decorate --graph";
+            ps = "!git push origin $(git rev-parse --abbrev-ref HEAD)";
+            pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
+            af = "!git add $(git ls-files -m -o --exclude-standard | fzf -m)";
+            hist = ''
+              log --pretty=format:"%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)" --graph --date=relative --decorate --all
+            '';
+            llog = ''
+              log --graph --name-status --pretty=format:"%C(red)%h %C(reset)(%cd) %C(green)%an %Creset%s %C(yellow)%d%Creset" --date=relative
+            '';
+            # https://github.com/arichtman/nix/blob/18f5613c2842e12e49350aeceace63863ad59244/modules/home/default-home/default.nix#L11
+            fuggit = "!git add . && git commit --amend --no-edit && git push --force";
+            # thanks @vbde for this
+            idc = "!git commit -am '$(curl -s https://whatthecommit.com/index.txt)'";
+          };
+
           init.defaultBranch = "main";
           repack.usedeltabaseoffset = "true";
           color.ui = "auto";
@@ -222,6 +214,19 @@ in
               # }
             ]
           );
+        };
+      };
+    }
+
+    {
+      # pager / diff tool
+      programs.delta = {
+        enable = true;
+
+        options = {
+          navigate = true;
+          side-by-side = true;
+          line-numbers = true;
         };
       };
     }
